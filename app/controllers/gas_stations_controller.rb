5545f0efe54d0d8404ec737f91763b9b537cf8d0
ronald <rvasquez@copec.cl>
2019-04-01 07:36:30 -0700

    20190401 1136 Autenticacion de usuario

--- app/controllers/gas_stations_controller.rb
+++ app/controllers/gas_stations_controller.rb
@@ -1,5 +1,7 @@
 class GasStationsController < ApplicationController
   before_action :set_gas_station, only: [:show, :edit, :update, :destroy]
+#Se agrego esta linea para decir que todo requiere autenticacion excepto Index
+  before_action :authenticate_user!, except: [:index]
 
   # GET /gas_stations
   # GET /gas_stations.json
